47d68438e41abfc5ad3fb49517449e7cea47ab48
amitjain <omertabib3004@gmail.com>
2019-06-30 15:36:33 +0530

    getting Started Prohect]

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/student-app'));
+app.use(express.static('./dist/studentApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+res.sendFile(path.join(__dirname,'/dist/studentApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
