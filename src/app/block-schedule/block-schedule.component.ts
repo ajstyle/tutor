2b543c2ea732836a9b1f4d94a9313f10e7f0cff5
amitjain <omertabib3004@gmail.com>
2019-07-01 22:45:55 +0530

    getting Started Prohect]

--- src/app/block-schedule/block-schedule.component.ts
+++ src/app/block-schedule/block-schedule.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit , ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
+import { Moment } from 'moment';
+import { MatCalendar } from '@angular/material';
 @Component({
   selector: 'app-block-schedule',
   templateUrl: './block-schedule.component.html',
   styleUrls: ['./block-schedule.component.scss']
 })
-export class BlockScheduleComponent implements OnInit {
+export class BlockScheduleComponent implements AfterViewInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  @ViewChild('calendar', {static : true}) calendar: MatCalendar<Moment>;
+  selectedDate: Moment;
+  constructor(private renderer: Renderer2) { }
+  monthSelected(date) {
+    // alert(`Selected: ${date}`);
   }
+
+  onDateChanged(date) {
+   // alert(`Selected: ${date}`);
+  }
+  ngAfterViewInit() {
+    // Find all arrow buttons in the calendar
+    const buttons = document.querySelectorAll('mat-calendar mat-calendar-header button');
+
+    if (buttons) {
+      // Listen for click event
+      Array.from(buttons).forEach(button => {
+        this.renderer.listen(button, 'click', () => {
+       //   alert('Arrow button clicked');
+        });
+      });
+    }
+  }
+
 
 }
